f2a502e3c9e3f056bd1157e143df8faa20ce6c87
Rich Eodice <rich@eodice.com>
2018-11-19 17:17:29 -0500

    Added basic api, DevicesController.cs.

---
+++ bwm-api/Controllers/v1/DevicesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+[ApiController]
+[Produces("application/json")]
+public class DevicesController : ControllerBase
+{
+    private const string dbServer = "192.168.50.225";
+    private const string dbName = "bwm";
+    private const string dbCollection = "devices";
+
+    [HttpGet]
+    [Route("api/v1/[controller]")]
+    public ActionResult GetOne()
+    {
+        var client = new MongoClient("mongodb://" + dbServer);
+        var database = client.GetDatabase(dbName);
+        var collection = database.GetCollection<Device>(dbCollection).AsQueryable();
+
+        var result = collection.ToList();
+
+        return Ok(result);
+    }
+
+    [HttpGet]
+    [Route("api/v1/[controller]/{id}")]
+    public ActionResult GetOne(Guid id)
+    {
+        var client = new MongoClient("mongodb://" + dbServer);
+        var database = client.GetDatabase(dbName);
+        var collection = database.GetCollection<Device>(dbCollection).AsQueryable();
+
+        var result = collection.Where(x => x.Id == id);
+
+        return Ok(result);
+    }
+
+    [HttpGet]
+    [Route("api/v1/[controller]/{id}/bw")]
+    public ActionResult GetDeviceBandwidths(Guid id, int take = 25, int skip = 0)
+    {
+        var client = new MongoClient("mongodb://" + dbServer);
+        var database = client.GetDatabase(dbName);
+        var collection = database.GetCollection<Device>(dbCollection).AsQueryable();
+        var site = collection.FirstOrDefault(x => x.Id == id);
+
+        var bandwidths = database.GetCollection<Bandwidth>(site.Site).AsQueryable();
+
+        var result = bandwidths
+            .Take(take)
+            .Skip(skip)
+            .ToList();
+
+        return Ok(result);
+    }
+}
--- bwm-svc/bwm-svc/Service1.cs
+++ bwm-svc/bwm-svc/Service1.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using SnmpSharpNet;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 public partial class Service1 : ServiceBase
 {
@@ -16,7 +14,7 @@ public partial class Service1 : ServiceBase
 
     //Service Timer Info
     private System.Timers.Timer m_mainTimer;
-    private int interval = 30 * 60000; //How often to run in milliseconds (minutes * 60000)
+    private int interval = 15 * 1000; //How often to run in milliseconds (seconds * 1000)
 
     public Service1()
     {
@@ -51,17 +49,300 @@ public partial class Service1 : ServiceBase
         Routine();
     }
 
-    private void Routine()
-    {
-    }
-
     public void OnDebug()
     {
+        //CreateDevices();
         //Manually kick off the service when debugging
         OnStart(null);
     }
 
     protected override void OnStop()
     {
+    }
+
+    private void Routine()
+    {
+        var devices = GetDevicesFromDb();
+        var results = new List<Bandwidth>();
+
+        foreach (var d in devices)
+        {
+            results.Add(GetBandwidth(d.Site, d.IPAddress));
+        }
+
+        foreach (var r in results)
+        {
+            WriteBandwidthToDb(r);
+        }
+    }
+
+    private static Bandwidth GetBandwidth(string Site, string IP)
+    {
+        try
+        {
+            var community = new OctetString("public");
+
+            var param = new AgentParameters(community)
+            {
+                Version = SnmpVersion.Ver1
+            };
+
+            var agent = new IpAddress(IP);
+
+            using (var target = new UdpTarget((IPAddress)agent, 161, 2000, 1))
+            {
+                var pdu = new Pdu(PduType.Get);
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.5.1");
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.10.1");
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.16.1");
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.5.2");
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.10.2");
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.16.2");
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.5.3");
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.10.3");
+                pdu.VbList.Add(".1.3.6.1.2.1.2.2.1.16.3");
+
+                var result = (SnmpV1Packet)target.Request(pdu, param);
+
+                if (result != null)
+                {
+                    // ErrorStatus other then 0 is an error returned by 
+                    // the Agent - see SnmpConstants for error definitions
+                    if (result.Pdu.ErrorStatus != 0)
+                    {
+                        // agent reported an error with the request
+                        Console.WriteLine("Error in SNMP reply. Error {0} index {1}",
+                            result.Pdu.ErrorStatus,
+                            result.Pdu.ErrorIndex);
+                    }
+                    else
+                    {
+                        var bw = new Bandwidth
+                        {
+                            Id = Guid.NewGuid(),
+                            DeviceIPAddress = IP,
+                            Site = Site,
+                            Timepstamp = DateTime.Now,
+                            X0Speed = Convert.ToDouble(result.Pdu.VbList[0].Value.ToString()),
+                            X0Ingress = Convert.ToDouble(result.Pdu.VbList[1].Value.ToString()),
+                            X0Egress = Convert.ToDouble(result.Pdu.VbList[2].Value.ToString()),
+                            X1Speed = Convert.ToDouble(result.Pdu.VbList[3].Value.ToString()),
+                            X1Ingress = Convert.ToDouble(result.Pdu.VbList[4].Value.ToString()),
+                            X1Egress = Convert.ToDouble(result.Pdu.VbList[5].Value.ToString()),
+                            X2Speed = Convert.ToDouble(result.Pdu.VbList[6].Value.ToString()),
+                            X2Ingress = Convert.ToDouble(result.Pdu.VbList[7].Value.ToString()),
+                            X2Egress = Convert.ToDouble(result.Pdu.VbList[8].Value.ToString())
+                        };
+
+                        return bw;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No response received from SNMP agent.");
+                }
+                target.Close();
+                return null;
+            }
+        }
+        catch { return null; }
+    }
+
+    private static List<Device> GetDevicesFromDb()
+    {
+        var _devices = new List<Device>();
+
+        try
+        {
+            var client = new MongoClient("mongodb://" + dbServer);
+            var database = client.GetDatabase(dbName);
+            var collection = database.GetCollection<Device>("devices").AsQueryable();
+
+            _devices.AddRange(collection.ToList());
+        }
+        catch { }
+
+        
+        return _devices;
+    }
+
+    private static void WriteBandwidthToDb(Bandwidth Bandwidth)
+    {
+        try
+        {
+            var client = new MongoClient("mongodb://" + dbServer);
+            var database = client.GetDatabase(dbName);
+
+            database.GetCollection<Bandwidth>(Bandwidth.Site).InsertOne(Bandwidth);
+        }
+        catch { }
+    }
+
+    private static void CreateDevices()
+    {
+        var client = new MongoClient("mongodb://" + dbServer);
+        var database = client.GetDatabase(dbName);
+        
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.1",
+            Site = "Edison"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.2",
+            Site = "New Brunswick"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.3",
+            Site = "Perth Amboy"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.4",
+            Site = "Somerville"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.5",
+            Site = "Newton"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.6",
+            Site = "Trenton"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.7",
+            Site = "Belvidere"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.8",
+            Site = "Paterson"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.9",
+            Site = "Morristown"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.10",
+            Site = "Flemington"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.11",
+            Site = "Jersey City"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.12",
+            Site = "Elizabeth"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.13",
+            Site = "Freehold"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.14",
+            Site = "Toms River"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.15",
+            Site = "Newark"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.17",
+            Site = "Atlantic City"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.18",
+            Site = "Cape May"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.19",
+            Site = "Hackensack"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.20",
+            Site = "Camden"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.21",
+            Site = "Vineland"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.22",
+            Site = "Mt. Holly"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.23",
+            Site = "Woodbury"
+        });
+
+        database.GetCollection<Device>("devices").InsertOne(new Device
+        {
+            Id = Guid.NewGuid(),
+            IPAddress = "172.16.8.99",
+            Site = "Cedar Knolls"
+        });
+
     }
 }
